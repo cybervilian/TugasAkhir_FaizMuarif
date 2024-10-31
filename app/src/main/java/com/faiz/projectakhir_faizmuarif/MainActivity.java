cd9c4f9828eb64047882e6ca8b6a7992db1e373f
faiz21s04 <faizkelas21s04@gmail.com>
2024-10-31 17:12:28 +0700

    commit

---
+++ app/src/main/java/com/faiz/projectakhir_faizmuarif/MainActivity.java
@@ -0,0 +1,14 @@
+package com.faiz.projectakhir_faizmuarif;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
